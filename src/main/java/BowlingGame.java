2dc4cc2ea61cf552cbf86e62f2b4037d7b7e07b9
ITRON\bjanoska <bence.janoska@itron.com>
2019-09-25 13:03:11 +0200

    Add: Implemented perfect game

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.*;
 
 public class BowlingGame {
+    
+    final private String rounds;
 
-    public BowlingGame(String scores){
-        if (!doesScoringPatternMatch(scores)) {
+    public BowlingGame(String rounds){
+        if (!doesScoringPatternMatch(rounds)) {
             throw new IllegalArgumentException("Scores doesn't match input pattern!");
         }
+
+        this.rounds = rounds;
     }
 
     private boolean doesScoringPatternMatch(String scores) {
@@ -27,7 +32,27 @@ public class BowlingGame {
     }
 
     public int result(){
-        return 0;
+        int sum = 0;
+        String[] scores = stripScores();
+        for(int index = 0; index < scores.length - 2; ++index){
+            String actScore = scores[index];
+            sum += scoreToValue(actScore);
+            if(actScore.equals("X")) {
+                sum += scoreToValue(scores[index + 1]);
+                sum += scoreToValue(scores[index + 2]);
+            }
+        }
+        return sum;
+    }
+
+    private String[] stripScores() {
+        return Arrays.stream(this.rounds.split(""))
+                .filter(stringValue -> !stringValue.equals("|"))
+                .toArray(String[]::new);
+    }
+
+    private int scoreToValue(String score){
+        return score.equals("X") ? 10 : Integer.parseInt(score);
     }
 
 }
